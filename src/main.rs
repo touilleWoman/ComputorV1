04c0e7f43491dd87230a0283d330cdcbc961198e
touilleWoman <jie.zhou.ac@gmail.com>
2022-01-24 22:12:45 +0100

    fix(struct): change Unit structure

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,33 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::env;
 use std::fmt;
+use std::vec;
 
+// struct Unit {
+//     a: f64,
+//     b: f64,
+//     c: f64,
+//     other: u64,
+// }
+type Unit = Vec<f64>;
 
-struct Unit {
-    a: f64,
-    b: f64,
-    c: f64,
-    other: u64,
-}
-
-impl fmt::Display for Unit {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let expression: String = vec![(self.c, "* X^0"), (self.b, "* X^1"), (self.a, "* X^2")]
-            .into_iter()
-            .map(|(x, y)| {
-                if x == 0.0 {
-                    "".to_string()
-                } else if x > 0.0 {
-                    format!("+ {} {}", x.to_string(), y)
-                } else {
-                    format!("- {} {}", (-x).to_string(), y)
-                }
-            })
-            .collect();
-        write!(f, "{} = 0", expression.trim_start_matches("+ "),)
-    }
-}
+// impl fmt::Display for Unit {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         let expression: String = vec![(self[0], "* X^0"), (self[1], "* X^1"), (self[2], "* X^2")]
+//             .into_iter()
+//             .map(|(x, y)| {
+//                 if x == 0.0 {
+//                     "".to_string()
+//                 } else if x > 0.0 {
+//                     format!("+ {} {}", x.to_string(), y)
+//                 } else {
+//                     format!("- {} {}", (-x).to_string(), y)
+//                 }
+//             })
+//             .collect();
+//         write!(f, "{} = 0", expression.trim_start_matches("+ "),)
+//     }
+// }
 
 fn reduce(expression: String) -> Result<Unit, &'static str> {
     let (left, right) = match expression.split_once("=") {
@@ -39,19 +40,20 @@ fn reduce(expression: String) -> Result<Unit, &'static str> {
     };
     let left_unit = extract_coefficients(left)?;
     let right_unit = extract_coefficients(right)?;
-    let reduced_unit = Unit {
-        a: left_unit.a - right_unit.a,
-        b: left_unit.b - right_unit.b,
-        c: left_unit.c - right_unit.c,
-        other: left_unit.other - right_unit.other,
+    let reduced_unit:Unit = left_unit.iter().zip(right_unit.iter()).map(|(&left,&right)| left - right).collect();
+    // Unit {
+    //     a: left_unit.a - right_unit.a,
+    //     b: left_unit.b - right_unit.b,
+    //     c: left_unit.c - right_unit.c,
+    //     other: left_unit.other - right_unit.other,
 
-    };
+    // };
     let degree = {
-        if reduced_unit.other != 0 {
-            reduced_unit.other
-        }else if reduced_unit.a != 0.0 {
+        if reduced_unit[3] != 0.0 {
+            reduced_unit[3] as u8
+        } else if reduced_unit[2] != 0.0 {
             2
-        } else if reduced_unit.b != 0.0 {
+        } else if reduced_unit[1] != 0.0 {
             1
         } else {
             0
@@ -59,7 +61,7 @@ fn reduce(expression: String) -> Result<Unit, &'static str> {
     };
 
     println!(
-        "Reduced form: {}\nPolynomial degree: {}",
+        "Reduced form: {:?}\nPolynomial degree: {}",
         reduced_unit, degree
     );
     if degree > 2 {
@@ -67,10 +69,6 @@ fn reduce(expression: String) -> Result<Unit, &'static str> {
     }
     Ok(reduced_unit)
 }
-
-// fn get_float(caps: &Captures, name: &str) -> f64 {
-//     caps.name(name).map_or(0.0, |m| m.as_str().parse::<f64>().unwrap())
-// }
 
 fn extract_coefficients(expression: &str) -> Result<Unit, &'static str> {
     lazy_static! {
@@ -89,12 +87,22 @@ fn extract_coefficients(expression: &str) -> Result<Unit, &'static str> {
     match RE.captures(expression) {
         None => Err("wrong expression, nothing captured"),
         Some(caps) => {
-            let unit = Unit {
-                c: caps.name("c").map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
-                b: caps.name("b").map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
-                a: caps.name("a").map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
-                other: caps.name("other").map_or(0, |m| m.as_str().parse::<u64>().unwrap()),
-            };
+            let unit:Unit = vec![
+                caps.name("c")
+                    .map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+                caps.name("b")
+                    .map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+                caps.name("a")
+                    .map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+                caps.name("other")
+                    .map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+            ];
+            // let unit = Unit {
+            //     c: caps.name("c").map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+            //     b: caps.name("b").map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+            //     a: caps.name("a").map_or(0.0, |m| m.as_str().parse::<f64>().unwrap()),
+            //     other: caps.name("other").map_or(0, |m| m.as_str().parse::<u64>().unwrap()),
+            // };
             Ok(unit)
         }
     }
@@ -105,26 +113,29 @@ fn remove_whitespace(s: &mut String) {
 }
 
 fn solve(data: Unit) {
-    if data.a == 0.0 {
+    let c = data[0];
+    let b = data[1];
+    let a = data[2];
+    if a == 0.0 {
         println!(
             "The solution is:\n{:1.8}",
-            PrettyPrintFloat(-data.c / data.b)
+            PrettyPrintFloat(-c / b)
         );
         return;
     }
 
-    let delta = data.b * data.b - 4.0 * data.a * data.c;
+    let delta = b * b - 4.0 * a * c;
     if delta < 0.0 {
         println!("Discriminant is negative, there is no solution.");
     } else if delta == 0.0 {
-        let solution = -data.b / (2.0 * data.a);
+        let solution = -b / (2.0 * a);
         println!(
             "Discriminant is zero, the one solution is:\n{:1.8}",
             PrettyPrintFloat(solution)
         );
     } else {
-        let solution1 = (-data.b + delta.sqrt()) / (2.0 * data.a);
-        let solution2 = (-data.b - delta.sqrt()) / (2.0 * data.a);
+        let solution1 = (-b + delta.sqrt()) / (2.0 * a);
+        let solution2 = (-b - delta.sqrt()) / (2.0 * a);
         println!(
             "Discriminant is strictly positive, the two solutions are:\n{:1.8}\n{:1.8}",
             PrettyPrintFloat(solution1),
@@ -141,7 +152,7 @@ fn usage(program: &str) {
     );
 }
 
-fn main()-> Result<(), &'static str> {
+fn main() -> Result<(), &'static str> {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     match args.len() {
